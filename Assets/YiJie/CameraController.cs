362596b97404b8defd02ce49165cbe57ae882a7c
aqew78 <ggfdagwshghxfg@gmail.com>
2024-04-04 23:55:40 +0800

    add game_scene

---
+++ Assets/YiJie/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed = 40f; // 視角移動速度
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0f).normalized;
+
+        Vector3 newPosition = transform.position + moveDirection * moveSpeed * Time.deltaTime;
+
+        //newPosition.z = -10f;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/YiJie/Castle.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Castle : MonoBehaviour
+{
+    [SerializeField] int hp = 10;
+    [SerializeField] Text hp_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            Debug.Log("hp--");
+            hp--;
+            updateHp_text();
+            Destroy(collision.gameObject);
+        }
+    }
+    void updateHp_text()
+    {
+        hp_text.text = hp.ToString();
+    }
+}
