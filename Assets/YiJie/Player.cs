fff2172613c60d8519cc61c7fdead0d6c4bf9a6c
aqew78 <ggfdagwshghxfg@gmail.com>
2024-04-18 20:47:00 +0800

    add createUI

--- Assets/YiJie/Player.cs
+++ Assets/YiJie/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour
     [SerializeField] Text iron_text;
     [SerializeField] Text coin_text;
     [SerializeField] Text food_text;
+    [SerializeField] Button buildButton;
+    [SerializeField] Image train_menu;
     private int wood = 0;
     private int stone = 0;
     private int iron = 0;
@@ -22,9 +24,12 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(1))
+        {
+            CreateMenuDetected();
+        }
     }
-   public  void changeWood(int number)
+    public void changeWood(int number)
     {
         wood += number;
         wood_text.text = wood.ToString();
@@ -48,5 +53,32 @@ public class Player : MonoBehaviour
     {
         food += number;
         food_text.text = food.ToString();
+    }
+    void CreateMenuDetected()
+    {
+        Vector3 inputPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //不要hit到Default層的場景
+        int charMask = LayerMask.GetMask("Default");
+        RaycastHit2D hit = Physics2D.Raycast(inputPosition, Vector2.zero, 10f, charMask);
+        if (!hit)
+        {
+            //hit場景，場景在layer3
+            int layerMask = LayerMask.GetMask("ground");
+            RaycastHit2D hitGround = Physics2D.Raycast(inputPosition, Vector2.zero, 10f, layerMask);
+            if (hitGround.collider != null)
+            {
+                if (hitGround.collider.tag == "ground")
+                {
+                    //點擊到地板，就顯示按鈕
+                    buildButton.gameObject.transform.position = new Vector3(inputPosition.x, inputPosition.y, 0f);
+                    buildButton.gameObject.SetActive(!buildButton.gameObject.activeSelf);
+                }
+            }
+        }
+    }
+    public void CreateButton()
+    {
+        buildButton.gameObject.SetActive(false);
+        train_menu.gameObject.SetActive(true);
     }
 }
