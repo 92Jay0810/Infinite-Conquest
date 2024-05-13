da1b9ebf056f0a0daa8f9e574134ecbef2b4b0d3
Harry Tsai <harry920628@gmail.com>
2024-05-13 17:16:23 +0800

    0513搜巡前

---
+++ Assets/XinZhang/Scripts/MultiRoomSceneManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text;
+using Photon.Realtime;
+
+public class MultiRoomSceneManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Text textRoomName;
+    [SerializeField] Text textPlayerList;
+
+
+    [SerializeField] Button buttonStartGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PhotonNetwork.CurrentRoom == null){
+            //Debug.LogError("No room found. Returning to StartScene.");
+            //PhotonNetwork.Disconnect();
+            SceneManager.LoadScene("StartScene");
+        }else{
+            Debug.Log("Current room: " + PhotonNetwork.CurrentRoom.Name);
+            textRoomName.text =  "當前房間： " + PhotonNetwork.CurrentRoom.Name;
+            UpdatePlayerList();
+        }
+        buttonStartGame.interactable = PhotonNetwork.IsMasterClient;
+        
+        
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+         buttonStartGame.interactable = PhotonNetwork.IsMasterClient;
+    }
+
+    public void UpdatePlayerList(){
+        StringBuilder sb = new StringBuilder();
+        foreach(var kvp  in PhotonNetwork.CurrentRoom.Players){
+            sb.AppendLine("→ " + kvp.Value.NickName);
+        }
+        textPlayerList.text = sb.ToString();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdatePlayerList();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdatePlayerList();
+    }
+
+    public void OnClickLeaveRoom(){
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public void OnClickStartGame(){
+        SceneManager.LoadScene("MultiGameScene");
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("MultiLobbyScene");
+    }
+}
